6bee448d70010bf23f6324596274b812f2fda0db
Mehedi Hasan <mehedisn3@gmail.com>
2023-08-18 16:21:57 +0600

    littile changes

--- index.js
+++ index.js
@@ -149,10 +149,12 @@ async function run(){
 
         app.get('/enrollment/:id', async(req, res) =>{
             const id = req.params.id;
+            console.log(id);
             const query = {_id: new ObjectId(id)}
             const result = await enrollmentCollection.findOne(query);
             res.send(result);
         });
+        
         app.put('/enrollment/:id', async(req, res) =>{
             const id = req.params.id;
             const filter = {_id: new ObjectId(id)}
