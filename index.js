2a8db44cee7697226b1f408a271c75aaf17b9a8d
minhaz-bin-monib <minhazbm463448@gmail.com>
2023-08-22 09:56:44 +0600

    finalize code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // user: dummyuser
@@ -23,85 +23,85 @@ const uri = "mongodb+srv://dummyuser:ExFGMzfCCY2aJPsV@cluster0.gaxjy.mongodb.net
 
 // Create a new MongoClient
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 
 
 
-async function run(){
-    try{
+async function run() {
+    try {
 
         const userCollection = client.db('nodeMongoCrud').collection('users');
         const serviceCollection = client.db('nodeMongoCrud').collection('services');
         const subscribersCollection = client.db('nodeMongoCrud').collection('subscribers');
-        const enquiryCollection = client.db('nodeMongoCrud').collection('enquiry'); 
+        const enquiryCollection = client.db('nodeMongoCrud').collection('enquiry');
         const enrollmentCollection = client.db('nodeMongoCrud').collection('enrollment');
         const aboutUSCollection = client.db('nodeMongoCrud').collection('AboutUs');
 
-           
-    // ************************************* PDF Generate ******************
-    app.post('/create-pdf', (req, res) => {
-        pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
-            if(err) {
-                res.send(Promise.reject());
-            }
-            
-            res.send(Promise.resolve());
+
+        // ************************************* PDF Generate ******************
+        app.post('/create-pdf', (req, res) => {
+            pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+                if (err) {
+                    res.send(Promise.reject());
+                }
+
+                res.send(Promise.resolve());
+            });
         });
-    });
-    
-    app.get('/fetch-pdf', (req, res) => {
-       
-        res.sendFile(`${__dirname}/result.pdf`)
-    })
+
+        app.get('/fetch-pdf', (req, res) => {
+
+            res.sendFile(`${__dirname}/result.pdf`)
+        })
 
 
 
         // ************************* Babysitter backend *******************
-        app.get('/users', async(req, res) =>{
+        app.get('/users', async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
             const users = await cursor.toArray();
             res.send(users);
         });
 
-        app.get('/users/:id', async(req, res) =>{
+        app.get('/users/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await userCollection.findOne(query);
             res.send(result);
         });
 
-        app.put('/users/:id', async(req, res) =>{
+        app.put('/users/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
             const updateUser = req.body;
             const eUser = {
 
                 $set: {
-                    name : updateUser.name,
-                    address : updateUser.address,
-                    email : updateUser.email,
-                    imageURL : updateUser.imageURL
+                    name: updateUser.name,
+                    address: updateUser.address,
+                    email: updateUser.email,
+                    imageURL: updateUser.imageURL
                 }
             }
 
             const result = await userCollection.updateOne(filter, eUser, options);
             res.send(result);
-            
+
         });
 
-        app.post('/users', async(req,res) =>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
             const result = await userCollection.insertOne(user)
             res.send(result);
         })
 
-        app.delete('/users/:id', async(req,res) =>{
+        app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             //console.log('trying to delete',id);
             const query = { _id: new ObjectId(id) }
@@ -117,18 +117,52 @@ async function run(){
 
 
 
-    // ************************* Services backend *******************
-        app.get('/services', async(req, res) =>{
+        // ************************* Services backend *******************
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
 
-        app.post('/services', async(req,res) =>{
+        app.post('/services', async (req, res) => {
             const services = req.body;
             console.log(services);
             const result = await serviceCollection.insertOne(services)
+            res.send(result);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.put('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updateUser = req.body;
+            const eUser = {
+
+                $set: {
+                    service_name: updateUser.service_name,
+                    service_details: updateUser.service_details
+                }
+            }
+
+            const result = await serviceCollection.updateOne(filter, eUser, options);
+            res.send(result);
+
+        });
+
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            //console.log('trying to delete',id);
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollection.deleteOne(query);
+            console.log(result);
             res.send(result);
         })
 
@@ -136,63 +170,63 @@ async function run(){
 
 
         // ************************* Subcribers backend *******************
-        app.get('/subscribers', async(req, res) =>{
+        app.get('/subscribers', async (req, res) => {
             const query = {};
             const cursor = subscribersCollection.find(query);
             const subscribers = await cursor.toArray();
             res.send(subscribers);
         });
 
-        app.post('/subscribers', async(req,res) =>{
+        app.post('/subscribers', async (req, res) => {
             let subscribers = req.body;
             console.log(subscribers);
             const result = await subscribersCollection.insertOne(subscribers)
             console.log(result);
             res.send(result);
-            
+
         })
 
 
         // ************************* Enrollment backend *******************
-        app.get('/enrollment', async(req, res) =>{
+        app.get('/enrollment', async (req, res) => {
             const query = {};
             const cursor = enrollmentCollection.find(query);
             const enrollment = await cursor.toArray();
             res.send(enrollment);
         });
 
-        app.post('/enrollment', async(req,res) =>{
+        app.post('/enrollment', async (req, res) => {
             let enrollment = req.body;
             console.log(enrollment);
             const result = await enrollmentCollection.insertOne(enrollment)
             console.log(result);
             res.send(result);
-            
+
         });
 
-        app.get('/enrollment/:id', async(req, res) =>{
+        app.get('/enrollment/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id);
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await enrollmentCollection.findOne(query);
             res.send(result);
         });
-        
-        app.put('/enrollment/:id', async(req, res) =>{
+
+        app.put('/enrollment/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
             const update = req.body;
             const eUser = {
 
-                 $set: {
-                    status : update.status,
-                 }
+                $set: {
+                    status: update.status,
+                }
             }
 
             const result = await enrollmentCollection.updateOne(filter, eUser, options);
             res.send(result);
-            
+
         });
 
 
@@ -200,59 +234,59 @@ async function run(){
 
 
         // ************************* enquiry backend *******************
-        app.get('/enquiry', async(req, res) =>{
+        app.get('/enquiry', async (req, res) => {
             const query = {};
             const cursor = enquiryCollection.find(query);
             const enquiry = await cursor.toArray();
             res.send(enquiry);
         });
 
-        app.post('/enquiry', async(req,res) =>{
+        app.post('/enquiry', async (req, res) => {
             let enquiry = req.body;
             console.log(enquiry);
             const result = await enquiryCollection.insertOne(enquiry)
             console.log(result);
             res.send(result);
-            
+
         })
 
-        app.get("/enquiry/:id", async(req, res) =>{
+        app.get("/enquiry/:id", async (req, res) => {
             const id = req.params.id;
             console.log(id);
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await enquiryCollection.findOne(query);
             res.send(result);
         });
-        
 
-        app.put('/enquiry/:id', async(req, res) =>{
+
+        app.put('/enquiry/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
             const update = req.body;
             const eUser = {
 
-                 $set: {
-                    status : update.status,
-                 }
+                $set: {
+                    status: update.status,
+                }
             }
 
             const result = await enquiryCollection.updateOne(filter, eUser, options);
             res.send(result);
-            
+
         });
 
 
 
         // ************************* AboutUs backend *******************
-        app.get('/aboutUs', async(req, res) =>{
+        app.get('/aboutUs', async (req, res) => {
             const query = {};
             const cursor = aboutUSCollection.find(query);
             const aboutUs = await cursor.toArray();
             res.send(aboutUs);
         });
 
-        app.post('/aboutUs', async(req,res) =>{
+        app.post('/aboutUs', async (req, res) => {
             const aboutUs = req.body;
             console.log(aboutUs);
             const result = await aboutUSCollection.insertOne(aboutUs)
@@ -264,7 +298,7 @@ async function run(){
 
     }
 
-    finally{
+    finally {
 
     }
 
@@ -275,12 +309,12 @@ run().catch(err => console.log(err));
 
 
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send('Hello from node mongo and crud');
 });
 
 
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Simple node server running on port ${port}`);
 })
